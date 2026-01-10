c1d5d63643c9beb8e46daed6903be09cbecd409f
schipet <schillpeet@gmail.com>
2026-01-11 00:29:37 +0100

    kata(debasing64): solved in java

---
+++ src/main/java/kata/Debasing64.java
@@ -0,0 +1,28 @@
+package kata;
+
+import java.util.Base64;
+import java.util.Scanner;
+
+/**
+ * Sample input                     Sample output
+ *
+ * bGF0ZS1hdC1uaWdodA==             late-at-night
+ * d2l0aC10aGUtcmlzaW5nLWFwZQ==     with-the-rising-ape
+ * dGhlLXJ1dGhsZXNzLXNldmVu         the-ruthless-seven
+ *
+ * Run the program and enter the sample input in the console
+ * CTRL+C will stop the program or just Type EXIT
+ */
+class Debasing64 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        Base64.Decoder decoder = Base64.getDecoder();
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            if ("EXIT".equals(input)) break;
+            String result = new String (decoder.decode(input));
+            System.out.println(result);
+        }
+        scanner.close();
+    }
+};
