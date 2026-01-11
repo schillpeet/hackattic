58395ba4e3ca54377fcf5d8ad36834c4a173b297
schipet <schillpeet@gmail.com>
2026-01-11 15:17:32 +0100

    kata(the_sum_of_things): solved in java

---
+++ src/main/java/kata/TheSumOfThings.java
@@ -0,0 +1,24 @@
+package kata;
+
+import java.util.Scanner;
+
+public class TheSumOfThings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] elements = line.split(" ");
+            int result = 0;
+            for (String el : elements) {
+                if (el.length() == 1 && !el.matches("[0-9]+")) result += (int) el.charAt(0); // ascii
+                else if (el.length() > 2 && el.charAt(0) == '0') {
+                    if (el.charAt(1) == 'o') result += Integer.parseInt(el.substring(2), 8);  // octal
+                    else if (el.charAt(1) == 'x') result += Integer.parseInt(el.substring(2), 16); // hex
+                    else if (el.charAt(1) == 'b') result += Integer.parseInt(el.substring(2), 2); // bin
+                } else result += Integer.parseInt(el, 10);
+            }
+            System.out.println(result);
+        }
+    }
+}
