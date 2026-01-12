0707a64df316c0ad8f8cf023239b98376efc3c14
schipet <schillpeet@gmail.com>
2026-01-12 12:35:57 +0100

    kata(yes_it_fizz): solved in python refactor

--- kata-others/kata-python/yes_it_fizzy.py
+++ kata-others/kata-python/yes_it_fizzy.py
@@ -1,19 +1,15 @@
 import fileinput
 
 def main():
-    #for line in fileinput.input():
-    #    print(line)
-    input_values = "8 16"
-    start, end = map(int, input_values.split())
-    for i in range(start, end+1):
-        fizz = i % 3 == 0
-        buzz = i % 5 == 0
-        if fizz and buzz: print('fizz')
-        elif fizz: print('fizz')
-        elif buzz: print('buzz')
-        else: print(i)
-
-
+    for line in fileinput.input():
+        n, m = map(int, line.split())
+        for i in range(n, m + 1):
+            fizz = i % 3 == 0
+            buzz = i % 5 == 0
+            if fizz and buzz: print('FizzBuzz')
+            elif fizz: print('Fizz')
+            elif buzz: print('Buzz')
+            else: print(i)
 
 if __name__ == "__main__":
     main()
