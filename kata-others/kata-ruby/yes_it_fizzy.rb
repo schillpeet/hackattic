0cd259c2fd471d073883d24b52919dbea39e0039
schipet <schillpeet@gmail.com>
2026-01-13 22:01:11 +0100

    fix(yes_it_fizzy): ruby spelling

--- kata-others/kata-ruby/yes_it_fizzy.rb
+++ kata-others/kata-ruby/yes_it_fizzy.rb
@@ -1,6 +1,6 @@
 STDIN.each do | line |
-  the_start, the_end = line.split.map(&:to_i)
-  (the_start..the_end).each do | el |
+  first, last = line.split.map(&:to_i)
+  (first..last).each do | el |
     res = "#{"Fizz" if el % 3 == 0}#{"Buzz" if el % 5 == 0}"
     puts res.empty? ? el : res
   end
