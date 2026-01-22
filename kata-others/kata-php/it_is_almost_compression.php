2fb228ef92843787696439a6f05ea6f3ee4b3764
schipet <schillpeet@gmail.com>
2026-01-22 21:19:11 +0100

    kata(it_is_almost_compression): solved with php

---
+++ kata-others/kata-php/it_is_almost_compression.php
@@ -0,0 +1,21 @@
+<?php
+
+$f = fopen( 'php://stdin', 'r' );
+
+
+
+while( $line = fgets( $f ) ) {
+    preg_match_all('/(.)\1*/', $line, $matches);
+    foreach ($matches[0] as $group) {
+        if (strlen($group) > 2) {
+            echo strlen($group) . $group[0];
+        } else {
+            echo $group;
+        }
+    }
+    echo PHP_EOL;
+}
+
+fclose( $f );
+
+?>
